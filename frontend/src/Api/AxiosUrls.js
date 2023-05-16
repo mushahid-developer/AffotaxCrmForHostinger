74b502b5340937adc557f0baaaa578450f1cde9a
unknown <mushahidzahid.developer@gmail.com>
2023-05-16 20:04:26 +0500

    m

--- frontend/src/Api/AxiosUrls.js
+++ frontend/src/Api/AxiosUrls.js
@@ -1,6 +1,6 @@
 //BaseUrl
 // export const BaseUrl = 'https://affotax-crm.herokuapp.com/api/'; // Live
-export const BaseUrl = 'http://192.168.18.59:7878/api/'; // Local
+export const BaseUrl = 'http://141.136.47.50:5000/api/'; // Local
 // 192.168.18.59
 
 //Auth / Users
