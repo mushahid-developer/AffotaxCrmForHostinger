962e858f0d21c00e9b4af49fbfbcbcfe36afa634
unknown <mushahidzahid.developer@gmail.com>
2023-05-16 21:02:34 +0500

    m

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use('/api', require('./server/routes/routes'));
 //     })
 // }
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
