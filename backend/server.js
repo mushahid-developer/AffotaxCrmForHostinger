416de8390679b6ace1d385ac55b65d706d471407
unknown <mushahidzahid.developer@gmail.com>
2023-05-16 22:33:32 +0500

    m

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/api', require('./server/routes/routes'));
 
 if ( process.env.NODE_ENV == "production"){
     
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
     })
