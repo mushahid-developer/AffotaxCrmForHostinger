74b6a246f99ef7d8e09219ac47a71a9bda138f07
unknown <mushahidzahid.developer@gmail.com>
2023-05-22 22:05:16 +0500

    m

--- backend/server/database/connection.js
+++ backend/server/database/connection.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
     try{
-        const con = await mongoose.connect(process.env.MONGO_URI,  {});
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useFindAndModify: false,
+            // useCreateIndex: true
+        });
 
         console.log(`MongoDB connceted: ${con.connection.host}`);
     }
