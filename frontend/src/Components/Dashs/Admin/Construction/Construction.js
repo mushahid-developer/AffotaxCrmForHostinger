c7d0638f7a02d08596ee00cc7112511f5f94937c
unknown <mushahidzahid.developer@gmail.com>
2023-05-27 19:14:28 +0500

    m

--- frontend/src/Components/Dashs/Admin/Construction/Construction.js
+++ frontend/src/Components/Dashs/Admin/Construction/Construction.js
@@ -390,9 +390,11 @@ const Construction = () => {
     }, rowData)
 
     const handleDeleteProject = async (projId)=>{
-      setLoader(true)
       const confirmed = window.confirm('Are you sure you want to delete this item?');
       if (confirmed) {
+
+        const filteredArray = rowData.filter(obj => obj._id !== projId);
+        setRowData(filteredArray)
 
         await axios.get(`${ProjDeleteUrl}/${projId}`,
             {
@@ -405,9 +407,11 @@ const Construction = () => {
     }
 
     const handleCompleteProject = async (projId)=>{
-      setLoader(true)
+
       const confirmed = window.confirm('Are you sure you want to mark it as Completed?');
       if (confirmed) {
+
+        
 
         await axios.get(`${ProjCompletedUrl}/${projId}`,
             {
@@ -419,8 +423,28 @@ const Construction = () => {
       }
     }
 
-    const handleCopyProject = async (projId)=>{
-      setLoader(true)
+    const handleCopyProject = async (projId, projData)=>{
+
+
+      const copiedProj = {
+        houseNoList_id: projData.houseNoList_id,
+        Task: projData.Task,
+        supervisor_id: projData.supervisor_id,
+        Jobholder_id: projData.Jobholder_id,
+        startDate: projData.startDate,
+        completationDate: projData.completationDate,
+        JobDate: projData.JobDate,
+        Note: projData.Note,
+        budgetPlan: projData.budgetPlan,
+        budgetActual: projData.budgetActual,
+        status: projData.status,
+        Manager: projData.Manager,
+        comments: projData.comments,
+        _id: `${projData._id}1001`
+      }
+      setRowData(prev => [...prev, copiedProj])
+
+
       await axios.get(`${ProjCopyUrl}/${projId}`,
           {
           headers:{ 'Content-Type': 'application/json' }
@@ -449,7 +473,9 @@ const Construction = () => {
       const columnDefs = [
         
         { headerName: 'House No', field: 'houseNoList_id', flex:2.5,
-        valueGetter: (params)=>{return(params.data.houseNoList_id && params.data.houseNoList_id.name)} ,
+        valueGetter: (params)=>{
+          return(params.data.houseNoList_id ? params.data.houseNoList_id.name ? params.data.houseNoList_id.name : params.data.houseNoList_id_name : params.data.houseNoList_id_name)
+        } ,
         cellEditor: 'agSelectCellEditor',
         cellEditorParams:  {
           values: filterFvalue === "Open" ? projectFOpenNames && projectFOpenNames.map(option => option.label) : projectFCloseNames && projectFOpenNames.map(option => option.label) ,
@@ -481,8 +507,8 @@ const Construction = () => {
           },
         },
         { headerName: 'Supervisor', field: 'supervisor_id', flex:2,
-        valueGetter: p => {
-          return p.data.supervisor_id !== null ? p.data.supervisor_id.name : ' ' //to get value from obj inside obj
+        valueGetter: params => {
+          return(params.data.supervisor_id ? params.data.supervisor_id.name ? params.data.supervisor_id.name : params.data.supervisor_id_name : params.data.supervisor_id_name)
         },
         cellEditor: 'agSelectCellEditor',
           cellEditorParams: {
@@ -501,7 +527,7 @@ const Construction = () => {
         },
         { headerName: 'Job Holder', field: 'Jobholder_id', flex:2,
         valueGetter: p => {
-          return p.data.Jobholder_id !== null ? p.data.Jobholder_id.name : ' ' //to get value from obj inside obj
+          return  p.data.Jobholder_id ? p.data.Jobholder_id.name ? p.data.Jobholder_id.name : p.data.Jobholder_id_name :p.data.Jobholder_id_name
         },
         cellEditor: 'agSelectCellEditor',
           cellEditorParams: {
@@ -659,8 +685,8 @@ const Construction = () => {
           }
         },
         { headerName: 'Manager', field: 'Manager', flex:2,
-          valueGetter: p => {
-            return p.data.Manager !== null ? p.data.Manager.name : ' ' //to get value from obj inside obj
+          valueGetter: params => {
+            return(params.data.Manager ? params.data.Manager.name ? params.data.Manager.name : params.data.Manager_name : params.data.Manager_name)
           },
           cellEditor: 'agSelectCellEditor',
           cellEditorParams: {
@@ -686,7 +712,7 @@ const Construction = () => {
           <>
           {params.data.isActive && 
           <> 
-            <Link onClick={()=>{handleCopyProject(params.data._id)}} style={{all: 'unset', cursor: 'pointer', textAlign: 'center !important'}}>
+            <Link onClick={()=>{handleCopyProject(params.data._id, params.data)}} style={{all: 'unset', cursor: 'pointer', textAlign: 'center !important'}}>
               <svg className="mx-1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 256 256">
                 <path d="M48.186 92.137c0-8.392 6.49-14.89 16.264-14.89s29.827-.225 29.827-.225-.306-6.99-.306-15.88c0-8.888 7.954-14.96 17.49-14.96 9.538 0 56.786.401 61.422.401 4.636 0 8.397 1.719 13.594 5.67 5.196 3.953 13.052 10.56 16.942 14.962 3.89 4.402 5.532 6.972 5.532 10.604 0 3.633 0 76.856-.06 85.34-.059 8.485-7.877 14.757-17.134 14.881-9.257.124-29.135.124-29.135.124s.466 6.275.466 15.15-8.106 15.811-17.317 16.056c-9.21.245-71.944-.49-80.884-.245-8.94.245-16.975-6.794-16.975-15.422s.274-93.175.274-101.566zm16.734 3.946l-1.152 92.853a3.96 3.96 0 0 0 3.958 4.012l73.913.22a3.865 3.865 0 0 0 3.91-3.978l-.218-8.892a1.988 1.988 0 0 0-2.046-1.953s-21.866.64-31.767.293c-9.902-.348-16.672-6.807-16.675-15.516-.003-8.709.003-69.142.003-69.142a1.989 1.989 0 0 0-2.007-1.993l-23.871.082a4.077 4.077 0 0 0-4.048 4.014zm106.508-35.258c-1.666-1.45-3.016-.84-3.016 1.372v17.255c0 1.106.894 2.007 1.997 2.013l20.868.101c2.204.011 2.641-1.156.976-2.606l-20.825-18.135zm-57.606.847a2.002 2.002 0 0 0-2.02 1.988l-.626 96.291a2.968 2.968 0 0 0 2.978 2.997l75.2-.186a2.054 2.054 0 0 0 2.044-2.012l1.268-62.421a1.951 1.951 0 0 0-1.96-2.004s-26.172.042-30.783.042c-4.611 0-7.535-2.222-7.535-6.482S152.3 63.92 152.3 63.92a2.033 2.033 0 0 0-2.015-2.018l-36.464-.23z" stroke="#979797" fill-rule="evenodd"/>
               </svg>
@@ -728,21 +754,25 @@ const Construction = () => {
         if(event.colDef.field === "Manager"){
           const selectedOption = fPreData.find(option => option.label === event.data.Manager);
           event.data.Manager = selectedOption ? selectedOption.value : '';
+          event.data.Manager_name = selectedOption ? selectedOption.label : '';
         }
         
         if(event.colDef.field === "Jobholder_id"){
           const selectedOption = fPreData.find(option => option.label === event.data.Jobholder_id);
           event.data.Jobholder_id = selectedOption ? selectedOption.value : '';
+          event.data.Jobholder_id_name = selectedOption ? selectedOption.label : '';
         }
         
         if(event.colDef.field === "supervisor_id"){
           const selectedOption = fPreData.find(option => option.label === event.data.supervisor_id);
           event.data.supervisor_id = selectedOption ? selectedOption.value : '';
+          event.data.supervisor_id_name = selectedOption ? selectedOption.label : '';
         }
 
         if(event.colDef.field === "houseNoList_id"){
           const selectedOption = projectFOpenNames && filterFvalue === "Open" ? projectFOpenNames ? projectFOpenNames.find(option => option.label === event.data.houseNoList_id) : " " : projectFCloseNames ?  projectFOpenNames.find(option => option.label === event.data.houseNoList_id) : "";
           event.data.houseNoList_id = selectedOption ? selectedOption.value : '';
+          event.data.houseNoList_id_name = selectedOption ? selectedOption.label : '';
         }
         console.log()
       }, [gridApi]);
@@ -761,9 +791,7 @@ const onRowValueChanged = useCallback(async (event) => {
         headers:{ 'Content-Type': 'application/json' }
       }
     );
-      console.log(resp)
-      console.log(i)
-      setLoader(true)
+      // setLoader(true)
       setReRender2(i)
       i = i + 1
 
