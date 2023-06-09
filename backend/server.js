860cff12af17b8456fa5a8c3180b28bd5fe2ddb8
unknown <mushahidzahid.developer@gmail.com>
2023-06-09 20:55:16 +0500

    m

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,11 @@ app.use(morgan('tiny'));
 // Load Routes
 app.use('/api', require('./server/routes/routes'));
 
-if ( process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve('../frontend/build/index.html'));
-    })
-}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+  
 
 
 app.get('*', function (req, res) {
