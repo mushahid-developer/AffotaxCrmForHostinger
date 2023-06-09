769ccfaa21e46872146cbaff891b4f561200a65d
unknown <mushahidzahid.developer@gmail.com>
2023-06-09 21:08:52 +0500

    m

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use('/api', require('./server/routes/routes'));
 
-pp.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
 );
