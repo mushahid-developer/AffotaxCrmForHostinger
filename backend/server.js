02667bc536ffdf235fb8e149c60cf038ddd2ec37
unknown <mushahidzahid.developer@gmail.com>
2023-06-09 21:03:55 +0500

    m

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(morgan('tiny'));
 app.use('/api', require('./server/routes/routes'));
 
 app.get("*", (req, res) => {
-  res.sendFile("../frontend/build/index.html");
+  res.sendFile( {path: "../frontend/build/index.html"});
 });
 
 app.get('*', function (req, res) {
