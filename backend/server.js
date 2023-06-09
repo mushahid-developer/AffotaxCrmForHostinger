f318d00703def95f9e0270e0be4ea55cbeb5f830
unknown <mushahidzahid.developer@gmail.com>
2023-06-09 21:00:57 +0500

    m

--- backend/server.js
+++ backend/server.js
@@ -1,62 +1,41 @@
-//Import
 const express = require("express");
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-var cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 
-// Import MongoDb Connection file
 const connectDB = require('./server/database/connection');
-
-//Import Cron Job file to run it
 require('./server/controller/RecurringTaskPing');
 
-// initialization App
-const app = express();
-
-// Setting Port Number
-dotenv.config({path:'../config.env'});
+dotenv.config({ path: '../config.env' });
 const PORT = process.env.PORT || 5000;
 
-// Mongo DB Connection
 connectDB();
 
-// // Json Parsing
+const app = express();
+
 app.use(bodyParser.json());
-
-// Cors
-app.use(cors())
-
-//Log Requests
+app.use(cors());
 app.use(morgan('tiny'));
-
-// Load Routes
 app.use('/api', require('./server/routes/routes'));
 
-
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-  });
-  
-
-
-app.get('*', function (req, res) {
-    res.send("404 Not Found");
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
+app.get('*', function (req, res) {
+  res.send("404 Not Found");
+});
 
-
-
-
-
-app.listen(PORT, ()=>{
-    console.log('************************************')
-    console.log(' ********************************** ')
-    console.log('  ********************************  ')
-    console.log('   ******************************   ')
-    console.log(`Server Started on http://localhost:${PORT}`)
-    console.log('   ******************************   ')
-    console.log('  ********************************  ')
-    console.log(' ********************************** ')
-    console.log('************************************')
-})
+app.listen(PORT, () => {
+  console.log('************************************');
+  console.log(' ********************************** ');
+  console.log('  ********************************  ');
+  console.log('   ******************************   ');
+  console.log(`Server Started on http://localhost:${PORT}`);
+  console.log('   ******************************   ');
+  console.log('  ********************************  ');
+  console.log(' ********************************** ');
+  console.log('************************************');
+});
