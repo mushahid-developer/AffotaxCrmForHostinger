c257959092c09a22ba4faa4faa58827707bd7732
unknown <mushahidzahid.developer@gmail.com>
2023-06-09 20:53:24 +0500

    m

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ if ( process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
     const path = require("path");
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+        res.sendFile(path.resolve('../frontend/build/index.html'));
     })
 }
 
