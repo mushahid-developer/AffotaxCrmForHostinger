50d0364ee594f46707e948064ccef457a77e63d4
mushahid-developer <mushahidzahid.developer@gmail.com>
2023-10-26 22:15:29 +0500

    m

--- frontend/src/Api/AxiosUrls.js
+++ frontend/src/Api/AxiosUrls.js
@@ -1,6 +1,6 @@
 //BaseUrl
-// export const BaseUrl = 'https://crm.affotax.com/api/'; // Live
-export const BaseUrl = 'http://localhost:7878/api/'; // Local
+export const BaseUrl = 'https://crm.affotax.com/api/'; // Live
+// export const BaseUrl = 'http://localhost:7878/api/'; // Local
 // 192.168.10.26
 
 //Auth / Users
